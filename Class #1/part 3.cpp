cd767acab21a66c39deb13f3579ccf0ecaf40961
Gustavo Bizo Jardim <69866294+gbiz0@users.noreply.github.com>
2023-03-30 11:35:36 -0300

    add class 1

---
+++ Class #1/part 3.cpp
@@ -0,0 +1,20 @@
+/*
+Using Variables
+*/
+#include <iostream> // <- Library
+
+using namespace std;
+int main() { // <- Function main
+	//Usualy the variables is created in the start of code
+	//After a main() function
+	int number;
+
+	//let's atribute a value for her
+	number = 25;
+
+	cout<<"The value of number is:" <<number;
+
+	
+    return 0; // <- Final code
+}
+
