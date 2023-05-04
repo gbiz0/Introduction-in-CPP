02e0db900f07148e192ba96f0238cee3d61f2078
gbiz0 <jardimbgustavo@gmail.com>
2023-05-04 09:35:39 -0300

    other commit

---
+++ Class #5 - Repeating Structure/exercise3.cpp
@@ -0,0 +1,20 @@
+/*Write a program that goes through a loop 100 times and prints only odd numbers*/
+
+#include <iostream>
+#include <string.h>
+#include <locale.h>
+using namespace std;
+
+int main (){
+    setlocale(LC_ALL, "Portuguese");
+
+    int i;
+
+    for (i = 1; i <= 100; i++){
+        if (!(i % 2 == 0)){
+            cout << i << "\n";
+        }
+    }
+    
+    return 0;
+}
